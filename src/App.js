d28de5ccf3ee7b79cf730b8b991e46459b3badf2
jiteshkrishnan <jitesh.krishnan@gmail.com>
2022-08-10 23:32:58 +0530

    added custom button

--- src/App.js
+++ src/App.js
@@ -5,19 +5,27 @@ export default function App() {
   const handleClick = () => {
     alert("have been clicked");
   };
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter" || event.key === " ") {
+      event.target?.click();
+    }
+  };
+
   return (
     <div className="App">
+      <br />
       <Button outline color="primary" onClick={handleClick}>
         primary button
       </Button>{" "}
       <a
-        role="button"
         tabIndex={0}
-        className="btn btn-outline-primary"
+        class="btn btn-outline-primary"
+        onKeyDown={handleKeyDown}
         onClick={handleClick}
       >
-        primary button
-      </a>{" "}
+        Primary button
+      </a>
     </div>
   );
 }
